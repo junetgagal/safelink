8f173a84e0e47537b8292424eb93bdf03af36521
junetgagal <34573624+junetgagal@users.noreply.github.com>
2017-12-15 19:32:31 +0700

    Update js.js

--- js.js
+++ js.js
@@ -132,7 +132,7 @@ function a_to_fa()
 {var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
 
 
-<script src='https://cdn.rawgit.com/dedi96/safelink/813ce72f/safelinkga.js' type='text/javascript'/>
+<script src='https://cdn.rawgit.com/junetgagal/safelink/e4301555/js.js' type='text/javascript'/>
 <script type='text/javascript'>
 protected_links = &quot;domain.com,blogspot.com,blogger.com,linkedin.com,facebook.com,twitter.com,google.com,feedburner.com&quot;;
 auto_safelink();
